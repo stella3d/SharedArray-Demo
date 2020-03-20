b206a60f17b15247eb3c48bb65e46bc87b8d5416
Stella Cannefax <stella@unity3d.com>
2020-03-20 04:08:51 -0700

    tweak help message

--- Assets/Editor/DemoScriptInspector.cs
+++ Assets/Editor/DemoScriptInspector.cs
@@ -5,7 +5,7 @@ namespace Stella3D.SharedArray.Demo
     [CustomEditor(typeof(SharedArrayMeshDrawerDemo))]
     public class SharedArrayMeshDrawerInspector : Editor
     {
-        const string k_Help = "Try changing the Transform above this,\nor the effect scaling parameters below!";
+        const string k_Help = "Try changing the Transform above this,\nor the effect parameters below!";
 
         public override void OnInspectorGUI()
         {
