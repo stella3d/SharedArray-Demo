a95f9bff7a30560b6866b98da9d9386979c4e570
Stella Cannefax <stella@unity3d.com>
2020-03-20 03:23:25 -0700

    add custom inspector with helpbox for demo script

---
+++ Assets/Editor/DemoScriptInspector.cs
@@ -0,0 +1,16 @@
+using UnityEditor;
+
+namespace Stella3D.SharedArray.Demo
+{
+    [CustomEditor(typeof(SharedArrayMeshDrawerDemo))]
+    public class SharedArrayMeshDrawerInspector : Editor
+    {
+        const string k_Help = "Try changing the Transform above this,\nor the effect scaling parameters below!";
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.HelpBox(k_Help, MessageType.Info);
+            DrawDefaultInspector();
+        }
+    }
+}
